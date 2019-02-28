d59e16d7c8a2f88dd533d7965d862bae6589adf3
Максим Рымарь <max_rymar@mail.ru>
2019-02-28 14:01:38 +0300

    Add files via upload

---
+++ src/SM.ml
@@ -0,0 +1,72 @@
+open GT       
+       
+(* The type for the stack machine instructions *)
+@type insn =
+(* binary operator                 *) | BINOP of string
+(* put a constant on the stack     *) | CONST of int                 
+(* read to stack                   *) | READ
+(* write from stack                *) | WRITE
+(* load a variable to the stack    *) | LD    of string
+(* store a variable from the stack *) | ST    of string with show
+
+(* The type for the stack machine program *)                                                               
+type prg = insn list
+
+(* The type for the stack machine configuration: a stack and a configuration from statement
+   interpreter
+ *)
+type config = int list * Syntax.Stmt.config
+
+(* Stack machine interpreter
+
+     val eval : config -> prg -> config
+
+   Takes a configuration and a program, and returns a configuration as a result
+ *)                         
+let eval_conf conf insn =
+	let (stack, state_cfg) = conf in
+	let (state, input, output) = state_cfg in
+	match insn with
+	    | BINOP operator -> (match stack with
+		    | y::x::tail -> ([(Syntax.Expr.operator operator) x y] @ tail, state_cfg))
+
+           | CONST value -> ([value] @ stack, state_cfg)
+
+	    | READ -> (match input with
+		    | head::tail -> ([head] @ stack, (state, tail, output)))
+
+	    | WRITE -> (match stack with
+		    | head::tail -> (tail, (state, input, output @ [head])))
+
+	    | LD  var -> ([state var] @ stack, statem_cfg)
+
+	    | ST  var -> (match stack with
+		    | head::tail -> (tail, (Syntax.Expr.update var head state, input, output)))
+
+let eval config prg = List.fold_left eval_config config prg
+
+(* Top-level evaluation
+
+     val run : int list -> prg -> int list
+
+   Takes an input stream, a program, and returns an output stream this program calculates
+*)
+let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
+
+(* Stack machine compiler
+
+     val compile : Syntax.Stmt.t -> prg
+
+   Takes a program in the source language and returns an equivalent program for the
+   stack machine
+ *)
+let rec compile_expr expr = match expr with
+       | Syntax.Expr.Const  const         -> [CONST const]
+       | Syntax.Expr.Var    var         -> [LD var]
+       | Syntax.Expr.Binop (op, le, ri) -> (compile_expr le) @ (compile_expr ri) @ [BINOP op]
+
+let rec compile s = match s with
+       | Syntax.Stmt.Read    var       -> [READ; ST var]
+       | Syntax.Stmt.Write   expr       -> (compile_expr expr) @ [WRITE]
+       | Syntax.Stmt.Assign (var, expr)   -> (compile_expr expr) @ [ST var]
+       | Syntax.Stmt.Seq    (le, ri) -> (compile le) @ (compile ri)
---
+++ src/Syntax.ml
@@ -0,0 +1,115 @@
+(* Opening a library for generic programming (https://github.com/dboulytchev/GT).
+   The library provides "@type ..." syntax extension and plugins like show, etc.
+*)
+open GT 
+    
+(* Simple expressions: syntax and semantics *)
+module Expr =
+  struct
+    
+    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
+       notation, it came from GT. 
+    *)
+    @type t =
+    (* integer constant *) | Const of int
+    (* variable         *) | Var   of string
+    (* binary operator  *) | Binop of string * t * t with show
+
+    (* Available binary operators:
+        !!                   --- disjunction
+        &&                   --- conjunction
+        ==, !=, <=, <, >=, > --- comparisons
+        +, -                 --- addition, subtraction
+        *, /, %              --- multiplication, division, reminder
+    *)
+                                                            
+    (* State: a partial map from variables to integer values. *)
+    type state = string -> int 
+
+    (* Empty state: maps every variable into nothing. *)
+    let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
+
+    (* Update: non-destructively "modifies" the state s by binding the variable x 
+      to value v and returns the new state.
+    *)
+    let update x v s = fun y -> if x = y then v else s y
+
+    (* Expression evaluator
+
+          val eval : state -> t -> int
+ 
+       Takes a state and an expression, and returns the value of the expression in 
+       the given state.
+    *)
+let toInt ret = if ret then 1 else 0
+let toBool ret = ret != 0
+    
+let operator new_oper left right = match new_oper with
+  | "+"  -> left + right
+  | "-"  -> left - right
+  | "*"  -> left * right
+  | "/"  -> left / right
+  | "%"  -> left mod right
+  | ">"  -> toInt (left > right)
+  | "<"  -> toInt (left < right)
+  | ">=" -> toInt (left >= right)
+  | "<=" -> toInt (left <= right)
+  | "==" -> toInt (left = right)
+  | "!=" -> toInt (left != right)
+  | "!!" -> toInt (toBool left || toBool right)
+  | "&&" -> toInt (toBool left && toBool right)
+
+  end
+  
+let rec eval state express = match express with
+  | Const c -> c
+  | Var v -> state v
+  | Binop (new_oper, left, right) -> operator new_oper (eval state left) (eval state right);;
+                   
+(* Simple statements: syntax and sematics *)
+module Stmt =
+  struct
+
+    (* The type for statements *)
+    @type t =
+    (* read into the variable           *) | Read   of string
+    (* write the value of an expression *) | Write  of Expr.t
+    (* assignment                       *) | Assign of string * Expr.t
+    (* composition                      *) | Seq    of t * t with show
+
+    (* The type of configuration: a state, an input stream, an output stream *)
+    type config = Expr.state * int list * int list 
+
+    (* Statement evaluator
+
+          val eval : config -> t -> config
+
+       Takes a configuration and a statement, and returns another configuration
+    *)
+                                                         
+  end
+  
+   let rec eval conf statement =
+      let (state, input, output) = conf in
+      match statement with
+        | Read var -> (match input with
+                      | head::tail -> (Expr.update var head state, tail, output))
+        | Write expr -> (state, input, output @ [Expr.eval state expr])
+        | Assign (var, expr) -> (Expr.update var (Expr.eval state expr) state, input, output)
+        | Seq (le, ri) -> eval (eval config le) ri;;  
+                                                         
+  end
+
+(* The top-level definitions *)
+
+(* The top-level syntax category is statement *)
+type t = Stmt.t    
+
+(* Top-level evaluator
+
+     eval : int list -> t -> int list
+
+   Takes a program and its input stream, and returns the output stream
+*)
+let eval i p =
+  let _, _, o = Stmt.eval (Expr.empty, i, []) p in o
